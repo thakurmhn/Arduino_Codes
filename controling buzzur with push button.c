3aa41d563c65bd8b44f4822538d3afb58a6e0df9
root <root@SYPULJG0V503.synechron.com>
2021-01-26 20:20:33 +0530

    First Commit

---
+++ controling buzzur with push button.c
@@ -0,0 +1,21 @@
+*/
+//Set the LED Pin constant globally
+//The +ve buzzer pin is connected to pin 9 of the Arduino
+#define buzzer 9
+//Connected to grounded resistor of switch
+#define ButtonPin 7
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  //set the Button Pin as an input
+  pinMode(ButtonPin, INPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  if (digitalRead(ButtonPin) == HIGH) {
+    tone(buzzer, 2000);   // sound the buzzer
+  } else {
+    noTone(buzzer);    // turn the buzzer off
+  }
+}
